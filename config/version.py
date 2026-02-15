f09d7ed852099c597746e4f15db195bfbf247a73
Luna the GSD <agit8or@agit8or.net>
2026-02-15 20:23:10 +0000

    Test v2.88.7

--- config/version.py
+++ config/version.py
@@ -2,11 +2,11 @@
 Version information for Client St0r
 """
 
-VERSION = '2.88.6'
+VERSION = '2.88.7'
 VERSION_INFO = {
     'major': 2,
     'minor': 88,
-    'patch': 6,
+    'patch': 7,
     'status': 'stable',  # alpha, beta, rc, stable
 }
 
