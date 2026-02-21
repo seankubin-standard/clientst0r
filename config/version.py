58641d60c31c8701b783b8c3e6e44391ef820acb
Luna the GSD <agit8or@agit8or.net>
2026-02-21 04:55:56 +0000

    Bump version to 3.7.1 - Drag-and-drop bug fix

--- config/version.py
+++ config/version.py
@@ -2,11 +2,11 @@
 Version information for Client St0r
 """
 
-VERSION = '3.7.0'
+VERSION = '3.7.1'
 VERSION_INFO = {
     'major': 3,
     'minor': 7,
-    'patch': 0,
+    'patch': 1,
     'status': 'stable',  # alpha, beta, rc, stable
 }
 
