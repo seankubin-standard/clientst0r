871acbb5760068cf83dc7a8e65f608f5103430b5
Luna the GSD <agit8or@agit8or.net>
2026-02-21 04:59:40 +0000

    Bump version to 3.7.2 - Enhanced drag-and-drop debugging

--- config/version.py
+++ config/version.py
@@ -2,11 +2,11 @@
 Version information for Client St0r
 """
 
-VERSION = '3.7.1'
+VERSION = '3.7.2'
 VERSION_INFO = {
     'major': 3,
     'minor': 7,
-    'patch': 1,
+    'patch': 2,
     'status': 'stable',  # alpha, beta, rc, stable
 }
 
