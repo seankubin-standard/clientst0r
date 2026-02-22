90220db9132318ea4813310bb9bc1ef7040f278b
Luna the GSD <agit8or@agit8or.net>
2026-02-22 00:30:25 +0000

    Bump version to 3.11.4 - OS Package Scanner automation release

--- config/version.py
+++ config/version.py
@@ -2,11 +2,11 @@
 Version information for Client St0r
 """
 
-VERSION = '3.11.3'
+VERSION = '3.11.4'
 VERSION_INFO = {
     'major': 3,
     'minor': 11,
-    'patch': 3,
+    'patch': 4,
     'status': 'stable',  # alpha, beta, rc, stable
 }
 
